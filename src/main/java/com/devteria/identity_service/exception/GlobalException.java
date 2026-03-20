1941603f97efb763ba6df27dd419a42389701245
DangTuanAnhh <dangtuananh2404@gmail.com>
2026-03-20 17:27:23 +0700

    lỗi 2

--- src/main/java/com/devteria/identity_service/exception/GlobalException.java
+++ src/main/java/com/devteria/identity_service/exception/GlobalException.java
@@ -1,5 +1,6 @@
 package com.devteria.identity_service.exception;
 
+import com.devteria.identity_service.dto.request.ApiReponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,10 +11,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 public class GlobalException {
 
     @ExceptionHandler(value = RuntimeException.class)
-     ResponseEntity<String> handLingRuntimeException(RuntimeException exception)  {
-        return  ResponseEntity.badRequest().body(exception.getMessage());
-        
+    ResponseEntity<ApiReponse<Object>> handLingRuntimeException(RuntimeException exception) {
+
+        ApiReponse<Object> apiReponse = new ApiReponse<>();
+        apiReponse.setCode(1000);
+        apiReponse.setMessage(exception.getMessage());
+        return ResponseEntity.badRequest().body(apiReponse);
     }
+
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
     ResponseEntity<String> handlingValidation(MethodArgumentNotValidException exception){
         return ResponseEntity.badRequest().body(exception.getFieldError().getDefaultMessage());
